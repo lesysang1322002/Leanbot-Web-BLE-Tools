79633f64d573796e92acdce170a272cbb88a2dd8
lesysang1322002 <128148253+lesysang1322002@users.noreply.github.com>
2025-10-05 13:50:53 +0700

    update

--- LbESP32-BLE-Uploader/script.js
+++ LbESP32-BLE-Uploader/script.js
@@ -1,3 +1,182 @@
+let LeanbotCharacteristic, WebTxCharacteristic, WebRxCharacteristic;
+let dev;
+let timeoutCheckMessage;
+
+let SERVICE_UUID        = '0000ffe0-0000-1000-8000-00805f9b34fb';
+let LEANBOT_UUID        = '0000ffe1-0000-1000-8000-00805f9b34fb';
+let WEB_TX_UUID         = '0000ffe2-0000-1000-8000-00805f9b34fb'; 
+let WEB_RX_UUID         = '0000ffe3-0000-1000-8000-00805f9b34fb'; 
+
+function isWebBluetoothEnabled() {
+  if (!navigator.bluetooth) {
+      console.log('Web Bluetooth API is not available in this browser!');
+      return false;
+  }
+  return true;
+}
+
+function requestBluetoothDevice() {
+  if (isWebBluetoothEnabled()){
+      logstatus('Finding...');
+      navigator.bluetooth.requestDevice({
+          filters: [{ services: [SERVICE_UUID] }],
+      })         
+      .then(device => {
+          device.addEventListener('gattserverdisconnected', onDisconnected);
+          dev = device;
+          logstatus("Connect to " + dev.name);
+          console.log('Connecting to', dev);
+          return device.gatt.connect();
+      })
+      .then(server => {
+          console.log('Getting GATT Service...');
+          logstatus('Getting Service...');
+          return server.getPrimaryService(SERVICE_UUID);
+      })
+      .then(service => {
+          console.log('Getting GATT Characteristics...');
+          logstatus('Getting Characteristics...');
+          return Promise.all([
+              service.getCharacteristic(LEANBOT_UUID),
+              service.getCharacteristic(WEB_TX_UUID),       
+              service.getCharacteristic(WEB_RX_UUID)        
+          ]);
+      })
+      .then(characteristics => {
+          logstatusWebName(dev.name);
+          UI("buttonText").innerText = "Rescan";
+
+          // Serial Monitor characteristic
+          LeanbotCharacteristic = characteristics[0];
+          LeanbotCharacteristic.addEventListener('characteristicvaluechanged', handleChangedValue);
+
+          // // TX (Write)
+          WebTxCharacteristic = characteristics[1];
+
+          // // RX (Notify)
+          WebRxCharacteristic = characteristics[2];
+          WebRxCharacteristic.addEventListener('characteristicvaluechanged', handleUploadRxChangedValue);
+
+          return Promise.all([
+              LeanbotCharacteristic.startNotifications(),
+              WebRxCharacteristic.startNotifications()
+          ]);
+      })
+      .catch(error => {
+          if (error instanceof DOMException && error.name === 'NotFoundError' && error.message === 'User cancelled the requestDevice() chooser.') {
+              console.log("User has canceled the device connection request.");
+              logstatus("SCAN to connect");
+          } else {
+              console.log("Unable to connect to device: " + error);
+              logstatus("ERROR");
+          }
+      });
+  }
+}
+
+let string = "";
+function handleUploadRxChangedValue(event) {
+    const data = event.target.value;
+    const dataArray = new Uint8Array(data.buffer);
+    const textDecoder = new TextDecoder('utf-8');
+    const valueString = textDecoder.decode(dataArray);
+
+    string += valueString;
+    const lines = string.split(/[\r\n]+/);
+    string = lines.pop() || "";
+    lines.forEach(line => {
+        if (line) { 
+            handleSerialLine(line);
+        }
+    });
+}
+
+function handleChangedValue(event) {
+  
+}
+
+async function sendBLE(data) {
+  if (!WebTxCharacteristic) {
+      console.log("GATT Characteristic not found.");
+      return;
+  }
+  data += '\n';  // Append newline character to data
+  console.log("You -> " + data);
+  let start = 0;
+  const dataLength = data.length;
+  while (start < dataLength) {
+    let subStr = data.substring(start, start + 20);
+    try {
+        let ByteStart = performance.now();
+        await WebTxCharacteristic.writeValueWithoutResponse(str2ab(subStr));
+        let ByteEnd = performance.now();
+        let ByteTime = ByteEnd - ByteStart;
+        console.log(`Time ${subStr.length} bytes: ${ByteTime.toFixed(2)} ms`);
+    } catch (error) {
+        console.error("Error writing to characteristic:", error);
+        break;
+    }
+    start += 20;
+  }
+}
+
+function checkMessageWithin5Seconds() {
+    // Thiết lập hàm setTimeout để kết thúc sau 5 giây
+    timeoutCheckMessage = setTimeout(function() {
+    console.log("5 seconds timeout, message incorrect.");
+    // Hiển thị info box
+    UI('infopopup').style.display = "block";
+    document.addEventListener("click", function(event) {
+        if (! infoBox.contains(event.target)) {
+            infoBox.style.display = "none";
+        }
+    });
+    }, 5000);
+}
+
+function logstatus(text){
+    UI('navbarTitle').textContent = text;
+}
+
+function disconnect()
+{
+    logstatus("SCAN to connect");
+    console.log("Disconnected from: " + dev.name);
+    return dev.gatt.disconnect();
+}
+
+function onDisconnected(event) {
+    const device = event.target;
+    logstatus("SCAN to connect");
+    resetVariable();
+    UI('buttonText').innerText = "Scan";
+    console.log(`Device ${device.name} is disconnected.`);
+}
+
+function str2ab(str)
+{
+    var buf = new ArrayBuffer(str.length);
+    var bufView = new Uint8Array(buf);
+    for (var i = 0, l = str.length; i < l; i++) {
+        bufView[i] = str.charCodeAt(i);
+    }
+    return buf;
+}
+
+function toggleFunction() {
+    if (UI('toggleButton').innerText == "Scan") {
+        requestBluetoothDevice();
+        return;
+    } 
+    disconnect();
+    requestBluetoothDevice();
+    resetVariable();
+}
+
+function UI(elmentID) {
+    return document.getElementById(elmentID);
+}
+
 clearTimeout(timeoutCheckMessage);
 
 function logstatusWebName(text){
@@ -55,59 +234,14 @@ dropZone.addEventListener("drop", (e) => {
   }
 });
 
-// Upload bằng hàm send()
-// document.getElementById("uploadBtn").addEventListener("click", async () => {
-//   if (!selectedFile) {
-//     alert("No file selected!");
-//     return;
-//   }
-
-//   const text = await selectedFile.text();
-//   const lines = text.split(/\r?\n/);
-
-//   for (let line of lines) {
-//     if (line.trim().length > 0) {
-//       await send(line);
-//       await new Promise(resolve => setTimeout(resolve, 10));
-//     }
-//   }
-
-//   alert("Send complete! LbESP32 will process the HEX file.");
-// });
-
-async function sendBLE(data) {
-  if (!gattCharacteristic) {
-      console.log("GATT Characteristic not found.");
-      return;
-  }
-  data += '\n';  // Append newline character to data
-  console.log("You -> " + data);
-  let start = 0;
-  const dataLength = data.length;
-  while (start < dataLength) {
-    let subStr = data.substring(start, start + 20);
-    try {
-        let ByteStart = performance.now();
-        await gattCharacteristic.writeValueWithoutResponse(str2ab(subStr));
-        await new Promise(resolve => setTimeout(resolve, 10)); // Thêm độ trễ nhỏ để tránh quá tải
-        let ByteEnd = performance.now();
-        let ByteTime = ByteEnd - ByteStart;
-        console.log(`Time ${subStr.length} bytes: ${ByteTime.toFixed(2)} ms`);
-    } catch (error) {
-        console.error("Error writing to characteristic:", error);
-        break;
-    }
-    start += 20;
-  }
-}
-
 document.getElementById("uploadBtn").addEventListener("click", async () => {
+  console.log("Upload button clicked");
   if (!selectedFile) {
     alert("No file selected!");
     return;
   }
 
-  await send("START SEND HEX LINES"); // Gửi lệnh bắt đầu
+  await sendBLE("START SEND HEX LINES"); // Gửi lệnh bắt đầu
   await new Promise(resolve => setTimeout(resolve, 1000)); // Đợi một chút để thiết bị chuẩn bị
   
 
@@ -121,7 +255,7 @@ document.getElementById("uploadBtn").addEventListener("click", async () => {
     if (line.trim().length > 0) {
       let lineStart = performance.now();
       await sendBLE(line);
-      await new Promise(resolve => setTimeout(resolve, 10)); // Độ trễ nhỏ giữa các dòng
+      await new Promise(resolve => setTimeout(resolve, 50)); // Độ trễ nhỏ giữa các dòng
       let lineEnd = performance.now();
       let lineTime = lineEnd - lineStart;
       console.log(`Time line: ${lineTime.toFixed(2)} ms`);
