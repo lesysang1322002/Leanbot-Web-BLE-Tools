7def19089f28b0301f765f87980e7448ef1e9da7
lesysang1322002 <128148253+lesysang1322002@users.noreply.github.com>
2025-10-31 17:19:44 +0700

    update

--- LeanbotBLE/main.js
+++ LeanbotBLE/main.js
@@ -2,29 +2,55 @@
 import { LeanbotBLE } from "./leanbot_ble.js";
 import * as utils from "https://cdn.jsdelivr.net/gh/lesysang1322002/Leanbot-Web-BLE-Tools/sdk_leanbot/leanbot_utils.js";
 
-// // =================== FILE SELECTION MODAL =================== //
-// const btnCode = document.getElementById("btnCode");
-// const modal = document.getElementById("fileModal");
-// const closeModal = document.getElementById("closeModal");
-// const fileNameLabel = document.getElementById("fileName");
+// =================== FILE SELECTION MODAL =================== //
+const btnCode = utils.UI("btnCode");
+const modal = utils.UI("fileModal");
+const closeModal = utils.UI("closeModal");
+const fileNameLabel = utils.UI("fileName");
+let loadedHexContent = ""; // lưu nội dung file HEX đã đọc
 
-// btnCode.addEventListener("click", () => {
-//   modal.classList.remove("hidden");
-// });
+btnCode.addEventListener("click", () => {
+  modal.classList.remove("hidden");
+});
 
-// closeModal.addEventListener("click", () => {
-//   modal.classList.add("hidden");
-// });
+closeModal.addEventListener("click", () => {
+  modal.classList.add("hidden");
+});
 
-// document.querySelectorAll(".fileOption").forEach(btn => {
-//   btn.addEventListener("click", e => {
-//     const filePath = e.target.getAttribute("data-file");
-//     const fileName = filePath.split("/").pop();
-//     fileNameLabel.textContent = fileName;
-//     modal.classList.add("hidden");
-//     utils.log(`Selected file: ${fileName}`);
-//   });
-// });
+document.querySelectorAll(".fileOption").forEach(btn => {
+  btn.addEventListener("click", async e => {
+    const filePath = e.target.getAttribute("data-file");
+    const fileName = filePath.split("/").pop();
+    fileNameLabel.textContent = fileName;
+    modal.classList.add("hidden");
+    utils.log(`Loaded HEX file: ${fileName}`);
+    const text = await fetch(filePath).then(res => res.text());
+    loadedHexContent = text;
+  });
+});
+
+// =================== Button Load HEX =================== //
+const btnLoadHex = utils.UI("btnLoadHex");
+const fileInput = utils.UI("hexFileInput");
+
+btnLoadHex.addEventListener("click", () => {
+  fileInput.click(); // mở hộp chọn file
+});
+
+fileInput.addEventListener("change", async (e) => {
+  const file = e.target.files[0];
+  if (!file) return;
+
+  // cập nhật tên file hiển thị
+  fileNameLabel.textContent = file.name;
+
+  // đọc nội dung HEX
+  const text = await file.text();
+  loadedHexContent = text;
+
+  console.log(`Loaded HEX file: ${file.name}`);
+});
+
 
 // =================== BLE =================== //
 
@@ -32,12 +58,10 @@ const status = utils.UI("leanbotStatus");
 const btnConnect = utils.UI("btnConnect");
 const btnReconnect = utils.UI("btnReconnect");
 
-// localStorage.removeItem("leanbot_device");
-
-const lastDevice = JSON.parse(localStorage.getItem("leanbot_device"));
-console.log("Last stored device for reconnect:", lastDevice);
-status.textContent = lastDevice ? lastDevice : "No Leanbot";
 const leanbot = new LeanbotBLE();
+console.log("Get last device for reconnect...");
+const lastDevice = leanbot.getLastLeanbotID();
+status.textContent = lastDevice ? lastDevice : "No Leanbot";
 
 leanbot.OnConnect = () => {
     status.textContent = leanbot.getLeanbotID();
@@ -52,10 +76,8 @@ btnConnect.onclick = async () => connectLeanbot();
 btnReconnect.onclick = async () => reconnectLeanbot();
 
 async function connectLeanbot() {
-    console.log("Disconnect before scan...");
-    leanbot.Disconnect();
     console.log("Scanning for Leanbot...");
-    const result = await leanbot.Connect();
+    const result = await leanbot.Rescan();
     console.log("Connect result:", result.message);
 }
 
@@ -64,38 +86,3 @@ async function reconnectLeanbot() {
     const result = await leanbot.Reconnect();
     console.log("Reconnect result:", result.message);
 }
-
-// utils.UI("btnConnect").onclick = () => {
-//   status.textContent = "Connecting...";
-//   status.className = "disconnected";
-//   utils.log("Connecting to Leanbot...");
-//   setTimeout(() => {
-//     status.textContent = "Leanbot 123456 BLE";
-//     status.className = "connected";
-//     utils.log("Connected to Leanbot 123456 BLE");
-//   }, 1000);
-// };
-
-// utils.UI("btnReconnect").onclick = () => {
-//   utils.log("Reconnect clicked (placeholder)");
-// };
-
-// // =================== SERIAL BUTTONS =================== //
-// utils.UI("btnClear").onclick = () => {
-//   utils.UI("serialLog").textContent = "";
-// };
-// utils.UI("btnCopy").onclick = () => {
-//   navigator.clipboard.writeText(utils.UI("serialLog").textContent);
-//   alert("Copied Serial Log!");
-// };
-
-// // =================== LOG FUNCTION =================== //
-// utils.log = function (msg) {
-//   const logBox = utils.UI("serialLog");
-//   const timestamp = utils.UI("showTimestamp").checked
-//     ? "[" + new Date().toLocaleTimeString() + "] "
-//     : "";
-//   logBox.textContent += timestamp + msg + "\n";
-//   if (utils.UI("autoScroll").checked)
-//     logBox.scrollTop = logBox.scrollHeight;
-// };
