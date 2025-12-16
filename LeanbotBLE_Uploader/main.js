47f59520741bf40fa8a48341eb06021c534afa82
lesysang1322002 <128148253+lesysang1322002@users.noreply.github.com>
2025-12-16 14:13:48 +0700

    Update main.js

--- LeanbotBLE_Uploader/main.js
+++ LeanbotBLE_Uploader/main.js
@@ -12,7 +12,7 @@ console.log(`BLE_MaxLength = ${window.BLE_MaxLength}`);
 console.log(`BLE_Interval = ${window.BLE_Interval}`);
 console.log(`HASH = ${window.HASH}`);
 
-localStorage.removeIteam("leanbot_device")
+localStorage.removeItem("leanbot_device");
 
 // =================== BLE Connection =================== //
 const leanbotStatus = document.getElementById("leanbotStatus");
